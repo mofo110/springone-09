32006071e84dc2a56a0352590ca3995a487db9bf
morton <morton_fojas@yahoo.com>
2017-12-04 02:21:28 -0800

    Initial commit.

---
+++ greeting-app/src/main/java/io/pivotal/training/GreetingApplication.java
@@ -0,0 +1,22 @@
+package io.pivotal.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GreetingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingApplication.class, args);
+  }
+
+}
